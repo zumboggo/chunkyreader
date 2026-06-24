7f07abceeb414870f6045db27b37abebd4b127aa
David Hepting <davidhepting@gmail.com>
2026-06-24 09:01:07 -0600

    Fix math level selection and refresh PWA cache

--- public/sw.js
+++ public/sw.js
@@ -1,4 +1,4 @@
-const CACHE_VERSION = 'chunky-learner-v10'
+const CACHE_VERSION = 'chunky-learner-v11'
 const STATIC_CACHE = `${CACHE_VERSION}-static`
 const RUNTIME_CACHE = `${CACHE_VERSION}-runtime`
 
