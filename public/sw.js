1f338718b78e8d5079a67c3caddca2506bbc95ad
David Hepting <davidhepting@gmail.com>
2026-07-29 04:15:27 +0800

    fix: bump SW cache version, add .nojekyll, rebuild to fix Sounds images

--- public/sw.js
+++ public/sw.js
@@ -1,4 +1,4 @@
-const CACHE_VERSION = 'chunky-learner-v11'
+const CACHE_VERSION = 'chunky-learner-v12'
 const STATIC_CACHE = `${CACHE_VERSION}-static`
 const RUNTIME_CACHE = `${CACHE_VERSION}-runtime`
 
